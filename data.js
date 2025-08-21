674ea698987fa29ec9c1a3c3df163619722fd99f
Vazy <35339440+vazy86@users.noreply.github.com>
2025-08-21 17:46:07 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_109_1",
+      "name": "Каб_109_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6656329552456253,
+        "pitch": 0.20170990079642337,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6367923243684714,
+          "pitch": 0.09767869953959263,
+          "rotation": 0,
+          "target": "1-_109_2"
+        },
+        {
+          "yaw": -0.2077025037964404,
+          "pitch": 0.08967999957584993,
+          "rotation": 0,
+          "target": "2-_109_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-_109_2",
+      "name": "Каб_109_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03350195591942651,
+          "pitch": 0.09196825081476767,
+          "rotation": 0,
+          "target": "2-_109_3"
+        },
+        {
+          "yaw": 2.8176371094025967,
+          "pitch": 0.11329471843701633,
+          "rotation": 0,
+          "target": "0-_109_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-_109_3",
+      "name": "Каб_109_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.401316324068137,
+          "pitch": 0.08586931815720611,
+          "rotation": 0,
+          "target": "0-_109_1"
+        },
+        {
+          "yaw": -1.8478277800428753,
+          "pitch": 0.08297249762894054,
+          "rotation": 0,
+          "target": "1-_109_2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Клиентская зона 21.08.25",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
